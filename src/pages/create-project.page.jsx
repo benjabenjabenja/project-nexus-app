7a65c87e8dc76dda9a82ff4c5da4e8d6b2163e47
bgonzalez <benjamin.gonzalez@ayi.group>
2024-03-07 20:46:57 -0300

    add db.json

--- src/pages/create-project.page.jsx
+++
@@ -1,7 +0,0 @@
-import { Layout } from '../compnents/layout';
-
-const CreateProject = () => {
-  return <h1>asdf</h1>
-};
-
-export default CreateProject;
---
+++ src/pages/gestion-projectos.page.jsx
@@ -0,0 +1,11 @@
+import { Layout } from '../compnents/layout';
+
+const GestionProjectos = () => {
+  return (
+    <>
+      
+    </>
+  )
+};
+
+export default GestionProjectos;
