2d60c05d2a54bbd4825771419a6d3f1f581109d1
benjamin gonzalez <jgonzalez.benjamin@gmail.com>
2024-03-12 11:21:41 -0300

    ajuste table task

--- src/compnents/tasks/table-task.jsx
+++ src/compnents/tasks/table-task.jsx
@@ -1,13 +1,29 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable react/prop-types */
 import { IconButton, Table, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
 import { generateUniqueId } from "../../helpers/unique_id";
+import { useEffect, useState } from "react";
 
 function TableTask(props) {
-    const { tasks, withActions, setActionClicked, setIdClicked } = props;
+    const { tasks, withActions } = props;
+    const [actionClicked, setActionClicked] = useState('');
+    const [idClicked, setIdClicked] = useState('');
+    const [taskEdited, setTaskEdited] = useState({});
+
+    useEffect(
+        () => { 
+            const edit_tasks = ({ id }) => {
+                // TODO: llamar al update_task.
+            }
+            edit_tasks({ id: idClicked  });
+        }, [actionClicked, idClicked]
+    );
+
     const handlerDelete = ev => {
         ev.preventDefault();
+        setIdClicked(tasks[0].id);
         setActionClicked('delete');
     }
 
@@ -39,29 +55,31 @@ function TableTask(props) {
                             {
                                 withActions && (
                                     <TableCell className={`${withActions ? 'block' : 'hidden'}`}>
-                    {withActions && (
-                        <>
-                            <IconButton
-                                aria-label="expand row"
-                                size="small"
-                                onClick={handlerEdit}
-                                color='primary'
-                                title={'edit project'}
-                            >
-                                <EditOutlinedIcon />
-                            </IconButton>
-                            <IconButton
-                                aria-label="expand row"
-                                size="small"
-                                onClick={handlerDelete}
-                                color='error'
-                                title={'delete project'}
-                            >
-                                <DeleteOutlineOutlinedIcon />   
-                            </IconButton>
-                        </>
-                    )}
-                </TableCell>
+                                        {
+                                            withActions && (
+                                                <>
+                                                    <IconButton
+                                                        aria-label="expand row"
+                                                        size="small"
+                                                        onClick={handlerEdit}
+                                                        color='primary'
+                                                        title={'edit project'}
+                                                    >
+                                                        <EditOutlinedIcon />
+                                                    </IconButton>
+                                                    <IconButton
+                                                        aria-label="expand row"
+                                                        size="small"
+                                                        onClick={handlerDelete}
+                                                        color='error'
+                                                        title={'delete project'}
+                                                    >
+                                                        <DeleteOutlineOutlinedIcon />   
+                                                    </IconButton>
+                                                </>
+                                            )
+                                        }
+                                    </TableCell>
                                 )
                             }
                         </TableRow>
